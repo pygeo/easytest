870444a9829ced7dbb355b35affd9021768eab08
loewalex <bulli92@users.noreply.github.com>
2017-02-24 13:17:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@
 from setuptools import setup
 from distutils.core import setup as setup_dist  # todo use only one setup
 
-import os
-import glob
+#import os
+#import glob
 
 # the setuptools are supposed to be used as a standard. Thats why we ommit
 # usage of distutils here
@@ -18,9 +18,9 @@ import glob
 # a small example how to build dependencies is given here:
 # http://stackoverflow.com/questions/11010151/distributing-a-shared-library-and-some-c-code-with-a-cython-extension-module
 
-import os
-import numpy as np
-import json
+#import os
+#import numpy as np
+#import json
 
 from setuptools import setup, Extension
 from setuptools import find_packages  # Always prefer setuptools over distutils
